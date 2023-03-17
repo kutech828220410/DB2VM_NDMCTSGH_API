fb6988381e7c618349adbdd885631acd2d4917b6
HS <HS@DESKTOP-QVKUL3S>
2023-03-17 17:20:19 +0800

    加入專案檔案。

---
+++ Controller/BBCMController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IBM.Data.DB2.Core;
+using System.Data;
+using System.Configuration;
+using Basic;
+using SQLUI;
+using System.Xml;
+namespace DB2VM.Controller
+{
+    [Route("dbvm/[controller]")]
+    [ApiController]
+    public class BBCMController : ControllerBase
+    {
+        public enum enum_雲端藥檔
+        {
+            GUID,
+            藥品碼,
+            中文名稱,
+            藥品名稱,
+            藥品學名,
+            健保碼,
+            包裝單位,
+            包裝數量,
+            最小包裝單位,
+            最小包裝數量,
+            藥品條碼1,
+            藥品條碼2,
+            警訊藥品,
+            管制級別,
+        }
+
+        static string MySQL_server = $"{ConfigurationManager.AppSettings["MySQL_server"]}";
+        static string MySQL_database = $"{ConfigurationManager.AppSettings["MySQL_database"]}";
+        static string MySQL_userid = $"{ConfigurationManager.AppSettings["MySQL_user"]}";
+        static string MySQL_password = $"{ConfigurationManager.AppSettings["MySQL_password"]}";
+        static string MySQL_port = $"{ConfigurationManager.AppSettings["MySQL_port"]}";
+
+        private SQLControl sQLControl_藥檔資料 = new SQLControl(MySQL_server, MySQL_database, "medicine_page_cloud", MySQL_userid, MySQL_password, (uint)MySQL_port.StringToInt32(), MySql.Data.MySqlClient.MySqlSslMode.None);
+
+
+        [HttpGet]
+        public string Get(string Code)
+        {
+            if (Code.StringIsEmpty()) return "[]";
+            System.Text.StringBuilder soap = new System.Text.StringBuilder();
+            soap.Append("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            soap.Append("<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">");
+            soap.Append("<soap:Body>");
+            soap.Append("<Drug_DATA xmlns=\"http://tempuri.org/\">");
+            soap.Append("<myhospital>1</myhospital>");
+            soap.Append("<myNS></myNS>");
+            soap.Append($"<myMCODE>{Code}</myMCODE>");
+            soap.Append("<myDB>OPD</myDB>");
+            soap.Append("</Drug_DATA>");
+            soap.Append("</soap:Body>");
+            soap.Append("</soap:Envelope>");
+            string Xml = Basic.Net.WebServicePost("https://tpord.mmh.org.tw/ADC_WS_A226/ADCDrugWS.asmx?op=Drug_DATA", soap);
+            string[] Node_array = new string[] { "soap:Body", "Drug_DATAResponse", "Drug_DATAResult", "diffgr:diffgram", "NewDataSet", "Temp1"};
+
+            XmlElement xmlElement = Xml.Xml_GetElement(Node_array);
+            string MCODE = xmlElement.Xml_GetInnerXml("MCODE");
+            string FullName = xmlElement.Xml_GetInnerXml("FULLNAME");
+            string ShortName = xmlElement.Xml_GetInnerXml("SHORTNAME");
+            string COMPAR2 = xmlElement.Xml_GetInnerXml("COMPAR2");
+            string MLEVEL = xmlElement.Xml_GetInnerXml("MLEVEL");
+            if (MCODE.StringIsEmpty()) return "[]";
+            List<MedClass> medClasses = new List<MedClass>();
+            MedClass medClass = new MedClass();
+            medClass.藥品碼 = MCODE;
+            medClass.藥品名稱 = FullName;
+            medClass.藥品學名 = ShortName;
+            medClass.警訊藥品 = (COMPAR2 == "Y") ? "True" : "False";
+            medClass.管制級別 = MLEVEL;
+
+            medClasses.Add(medClass);
+
+            List<object[]> list_藥檔資料 = sQLControl_藥檔資料.GetRowsByDefult(null, (int)enum_雲端藥檔.藥品碼, MCODE);
+            if(list_藥檔資料.Count == 0)
+            {
+                object[] value = new object[new enum_雲端藥檔().GetLength()];
+                value[(int)enum_雲端藥檔.GUID] = Guid.NewGuid().ToString();
+                value[(int)enum_雲端藥檔.藥品碼] = medClass.藥品碼;
+                value[(int)enum_雲端藥檔.藥品名稱] = medClass.藥品名稱;
+                value[(int)enum_雲端藥檔.藥品學名] = medClass.藥品學名;
+                value[(int)enum_雲端藥檔.警訊藥品] = medClass.警訊藥品;
+                value[(int)enum_雲端藥檔.管制級別] = medClass.管制級別;
+                sQLControl_藥檔資料.AddRow(null, value);
+            }
+            else
+            {
+                object[] value = list_藥檔資料[0];
+                value[(int)enum_雲端藥檔.藥品碼] = medClass.藥品碼;
+                value[(int)enum_雲端藥檔.藥品名稱] = medClass.藥品名稱;
+                value[(int)enum_雲端藥檔.藥品學名] = medClass.藥品學名;
+                value[(int)enum_雲端藥檔.警訊藥品] = medClass.警訊藥品;
+                value[(int)enum_雲端藥檔.管制級別] = medClass.管制級別;
+                List<object[]> list = new List<object[]>();
+                list.Add(value);
+                sQLControl_藥檔資料.UpdateByDefulteExtra(null, list);
+            }
+            //while (reader.Read())
+            //{
+            //    MedClass medClass = new MedClass();
+            //    medClass.藥品碼 = reader["UDDRGNO"].ToString().Trim();
+            //    medClass.藥品名稱 = reader["UDARNAME"].ToString().Trim();
+            //    medClass.料號 = reader["UDSTOKNO"].ToString().Trim();
+            //    medClass.ATC主碼 = reader["UDATC"].ToString().Trim();
+            //    medClass.藥品條碼1 = reader["UDBARCD1"].ToString().Trim();
+            //    medClass.藥品條碼2 = reader["UDBARCD2"].ToString().Trim();
+            //    medClass.藥品條碼3 = reader["UDBARCD3"].ToString().Trim();
+            //    medClass.藥品條碼4 = reader["UDBARCD4"].ToString().Trim();
+            //    medClass.藥品條碼5 = reader["UDBARCD5"].ToString().Trim();
+
+
+            //    medClasses.Add(medClass);
+            //}
+
+            if (medClasses.Count == 0) return "[]";
+            string jsonString = medClasses.JsonSerializationt();
+            return jsonString;
+        }
+    }
+}
---
+++ Controller/OutTakeMedController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IBM.Data.DB2.Core;
+using System.Data;
+using System.Configuration;
+using Basic;
+using Oracle.ManagedDataAccess.Client;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+namespace DB2VM_API
+{
+    public class class_OutTakeMed_data
+    {
+        [JsonPropertyName("MC_name")]
+        public string 電腦名稱 { get; set; }
+        [JsonPropertyName("code")]
+        public string 藥品碼 { get; set; }
+        [JsonPropertyName("value")]
+        public string 交易量 { get; set; }
+        [JsonPropertyName("operator")]
+        public string 操作人 { get; set; }
+        [JsonPropertyName("ID")]
+        public string ID { get; set; }
+        [JsonPropertyName("patient_name")]
+        public string 病人姓名 { get; set; }
+        [JsonPropertyName("patient_code")]
+        public string 病歷號 { get; set; }
+        [JsonPropertyName("prescription_time")]
+        public string 開方時間 { get; set; }
+        [JsonPropertyName("OP_type")]
+        public string 功能類型 { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OutTakeMedController
+    {
+        [Route("Sample")]
+        [HttpGet()]
+        public string Get_Sample()
+        {
+            string str = Basic.Net.WEBApiGet(@"http://10.14.16.50:443/api/OutTakeMed/Sample");
+
+            return str;
+        }
+        [HttpPost]
+        public string Post([FromBody] List<class_OutTakeMed_data> data)
+        {
+            string json = data.JsonSerializationt();
+            string str = Basic.Net.WEBApiPostJson("http://10.14.16.50:443/api/OutTakeMed/", json);
+            return str;
+        }
+    }
+}
