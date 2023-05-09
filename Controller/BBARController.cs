a7a4373ff50f739b44ea1e91b9aa46badb707975
HS <HS@DESKTOP-QVKUL3S>
2023-05-09 13:35:48 +0800

    1

--- Controller/BBARController.cs
+++ Controller/BBARController.cs
@@ -137,5 +137,95 @@ namespace DB2VM
 
 
         }
+
+
+        [Route("full")]
+        [HttpGet]
+        public string Getfull(string BarCode)
+        {
+            if (BarCode.StringIsEmpty()) return $"BarCode value failed! {BarCode}";
+            if (BarCode.Length < 5) return $"BarCode value failed! {BarCode}";
+            List<object[]> list_藥品資料 = this.sQLControl_UDSDBBCM.GetAllRows(null);
+
+            string[] BarCode_Ary = BarCode.Split(';');
+
+            if (BarCode_Ary.Length < 5) return "BarCode error!";
+            string 藥品碼 = BarCode_Ary[0].Substring(BarCode_Ary[0].Length - 5, 5);
+            string 日期_temp = BarCode_Ary[1];
+            string 病人姓名 = BarCode_Ary[2];
+            string 病歷號 = BarCode_Ary[3];
+            string 交易量 = BarCode_Ary[4];
+            if(交易量.StringIsInt32() == false)
+            {
+                return $"交易量 error!{交易量}";
+            }
+            交易量 = (交易量.StringToInt32() * -1).ToString();
+            if (日期_temp.Length != 7)
+            {
+                return $"Date error!{日期_temp}";
+            }
+            else
+            {
+                string Year = 日期_temp.Substring(0, 3);
+                string Month = 日期_temp.Substring(3, 2);
+                string Day = 日期_temp.Substring(5, 2);
+                Year = (Year.StringToInt32() + 1911).ToString();
+                日期_temp = $"{Year}/{Month}/{Day}";
+            }
+
+            list_藥品資料 = list_藥品資料.GetRows((int)enum_藥品資料_藥檔資料.藥品碼, 藥品碼);
+            if (list_藥品資料.Count == 0) return $"找不到此藥品 {藥品碼}";
+
+            List<object[]> list_value_Add = new List<object[]>();
+            List<OrderClass> orderClasses = new List<OrderClass>();
+            OrderClass orderClass = new OrderClass();
+            orderClass.PRI_KEY = Guid.NewGuid().ToString();
+            orderClass.藥局代碼 = "OPD";
+            orderClass.處方序號 = "";
+            orderClass.藥袋條碼 = $"{BarCode}";
+            orderClass.藥品碼 = 藥品碼;
+            orderClass.藥品名稱 = list_藥品資料[0][(int)enum_藥品資料_藥檔資料.藥品名稱].ObjectToString();
+            orderClass.病人姓名 = 病人姓名;
+            orderClass.病歷號 = 病歷號;
+            orderClass.包裝單位 = list_藥品資料[0][(int)enum_藥品資料_藥檔資料.包裝單位].ObjectToString();
+            orderClass.劑量 = "";
+            orderClass.頻次 = "";
+            orderClass.途徑 = "";
+            orderClass.天數 = "";
+            orderClass.交易量 = 交易量;
+            orderClass.開方時間 = DateTime.Now.ToDateTimeString();
+            orderClasses.Add(orderClass);
+
+            for (int i = 0; i < orderClasses.Count; i++)
+            {
+                List<object[]> list_value = this.sQLControl_醫囑資料.GetRowsByDefult(null, enum_醫囑資料.PRI_KEY.GetEnumName(), orderClasses[i].PRI_KEY);
+                if (list_value.Count == 0)
+                {
+                    object[] value = new object[new enum_醫囑資料().GetLength()];
+                    value[(int)enum_醫囑資料.GUID] = Guid.NewGuid().ToString();
+                    value[(int)enum_醫囑資料.PRI_KEY] = orderClasses[i].PRI_KEY;
+                    value[(int)enum_醫囑資料.藥局代碼] = orderClasses[i].藥局代碼;
+                    value[(int)enum_醫囑資料.藥品碼] = orderClasses[i].藥品碼;
+                    value[(int)enum_醫囑資料.藥品名稱] = orderClasses[i].藥品名稱;
+                    value[(int)enum_醫囑資料.病歷號] = orderClasses[i].病歷號;
+                    value[(int)enum_醫囑資料.藥袋條碼] = orderClasses[i].藥袋條碼;
+                    value[(int)enum_醫囑資料.病人姓名] = orderClasses[i].病人姓名;
+                    value[(int)enum_醫囑資料.交易量] = orderClasses[i].交易量;
+                    value[(int)enum_醫囑資料.開方日期] = orderClasses[i].開方時間;
+                    value[(int)enum_醫囑資料.產出時間] = DateTime.Now.ToDateTimeString_6();
+                    value[(int)enum_醫囑資料.過帳時間] = DateTime.MinValue.ToDateTimeString_6();
+                    value[(int)enum_醫囑資料.狀態] = "未過帳";
+                    list_value_Add.Add(value);
+                }
+            }
+
+            if (list_value_Add.Count > 0)
+            {
+                this.sQLControl_醫囑資料.AddRows(null, list_value_Add);
+            }
+            return orderClasses.JsonSerializationt();
+
+
+        }
     }
 }
