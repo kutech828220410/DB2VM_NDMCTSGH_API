63a8d50b959eb47c1279f2b814030970e5e3e7f3
HS <HS@DESKTOP-QVKUL3S>
2023-10-11 16:01:50 +0800

    20231011

--- Controller/BBARController.cs
+++ Controller/BBARController.cs
@@ -10,28 +10,14 @@ using System.Configuration;
 using Basic;
 using SQLUI;
 using Oracle.ManagedDataAccess.Client;
+using HIS_DB_Lib;
 namespace DB2VM
 {
     [Route("dbvm/[controller]")]
     [ApiController]
     public class BBARController : ControllerBase
     {
-        public enum enum_醫囑資料
-        {
-            GUID,
-            PRI_KEY,
-            藥局代碼,
-            藥袋條碼,
-            藥品碼,
-            藥品名稱,
-            病人姓名,
-            病歷號,
-            交易量,
-            開方日期,
-            產出時間,
-            過帳時間,
-            狀態,
-        }
+       
         public enum enum_急診藥袋
         {
             本次領藥號,
@@ -44,33 +30,16 @@ namespace DB2VM
             前次領藥號,
             本次醫令序號,
         }
-        public enum enum_藥品資料_藥檔資料
-        {
-            GUID,
-            藥品碼,
-            藥品中文名稱,
-            藥品名稱,
-            藥品學名,
-            藥品群組,
-            健保碼,
-            藥品條碼,
-            包裝單位,
-            庫存,
-            安全庫存,
-            圖片網址,
-            警訊藥品,
-        }
-
 
 
 
         static string MySQL_server = $"{ConfigurationManager.AppSettings["MySQL_server"]}";
-        static string MySQL_database = $"{ConfigurationManager.AppSettings["MySQL_DPS01_database"]}";
+        static string MySQL_database = $"{ConfigurationManager.AppSettings["MySQL_database"]}";
         static string MySQL_userid = $"{ConfigurationManager.AppSettings["MySQL_user"]}";
         static string MySQL_password = $"{ConfigurationManager.AppSettings["MySQL_password"]}";
         static string MySQL_port = $"{ConfigurationManager.AppSettings["MySQL_port"]}";
 
-        private SQLControl sQLControl_UDSDBBCM = new SQLControl(MySQL_server, MySQL_database, "medicine_page", MySQL_userid, MySQL_password, (uint)MySQL_port.StringToInt32(), MySql.Data.MySqlClient.MySqlSslMode.None);
+        private SQLControl sQLControl_UDSDBBCM = new SQLControl(MySQL_server, "dps01", "medicine_page", MySQL_userid, MySQL_password, (uint)MySQL_port.StringToInt32(), MySql.Data.MySqlClient.MySqlSslMode.None);
         private SQLControl sQLControl_醫囑資料 = new SQLControl(MySQL_server, MySQL_database, "order_list", MySQL_userid, MySQL_password, (uint)MySQL_port.StringToInt32(), MySql.Data.MySqlClient.MySqlSslMode.None);
 
         [HttpGet]
@@ -91,40 +60,36 @@ namespace DB2VM
             OrderClass orderClass = new OrderClass();
             orderClass.PRI_KEY = Guid.NewGuid().ToString();
             orderClass.藥局代碼 = "OPD";
-            orderClass.處方序號 = "";
             orderClass.藥袋條碼 = $"{BarCode}";
             orderClass.藥品碼 = 藥品碼;
             orderClass.藥品名稱 = list_藥品資料[0][(int)enum_藥品資料_藥檔資料.藥品名稱].ObjectToString();
             orderClass.病人姓名 = "";
             orderClass.病歷號 = "";
-            orderClass.包裝單位 = list_藥品資料[0][(int)enum_藥品資料_藥檔資料.包裝單位].ObjectToString();
-            orderClass.劑量 = "";
             orderClass.頻次 = "";
             orderClass.途徑 = "";
-            orderClass.天數 = "";
             orderClass.交易量 = num.ToString();
-            orderClass.開方時間 = DateTime.Now.ToDateTimeString();
+            orderClass.開方日期 = DateTime.Now.ToDateTimeString();
             orderClasses.Add(orderClass);
 
             for (int i = 0; i < orderClasses.Count; i++)
             {
-                List<object[]> list_value = this.sQLControl_醫囑資料.GetRowsByDefult(null, enum_醫囑資料.PRI_KEY.GetEnumName(), orderClasses[i].PRI_KEY);
+                List<object[]> list_value = this.sQLControl_醫囑資料.GetRowsByDefult(null, enum_醫令資料.PRI_KEY.GetEnumName(), orderClasses[i].PRI_KEY);
                 if (list_value.Count == 0)
                 {
-                    object[] value = new object[new enum_醫囑資料().GetLength()];
-                    value[(int)enum_醫囑資料.GUID] = Guid.NewGuid().ToString();
-                    value[(int)enum_醫囑資料.PRI_KEY] = orderClasses[i].PRI_KEY;
-                    value[(int)enum_醫囑資料.藥局代碼] = orderClasses[i].藥局代碼;
-                    value[(int)enum_醫囑資料.藥品碼] = orderClasses[i].藥品碼;
-                    value[(int)enum_醫囑資料.藥品名稱] = orderClasses[i].藥品名稱;
-                    value[(int)enum_醫囑資料.病歷號] = orderClasses[i].病歷號;
-                    value[(int)enum_醫囑資料.藥袋條碼] = orderClasses[i].藥袋條碼;
-                    value[(int)enum_醫囑資料.病人姓名] = orderClasses[i].病人姓名;
-                    value[(int)enum_醫囑資料.交易量] = orderClasses[i].交易量;
-                    value[(int)enum_醫囑資料.開方日期] = orderClasses[i].開方時間;
-                    value[(int)enum_醫囑資料.產出時間] = DateTime.Now.ToDateTimeString_6();
-                    value[(int)enum_醫囑資料.過帳時間] = DateTime.MinValue.ToDateTimeString_6();
-                    value[(int)enum_醫囑資料.狀態] = "未過帳";
+                    object[] value = new object[new enum_醫令資料().GetLength()];
+                    value[(int)enum_醫令資料.GUID] = Guid.NewGuid().ToString();
+                    value[(int)enum_醫令資料.PRI_KEY] = orderClasses[i].PRI_KEY;
+                    value[(int)enum_醫令資料.藥局代碼] = orderClasses[i].藥局代碼;
+                    value[(int)enum_醫令資料.藥品碼] = orderClasses[i].藥品碼;
+                    value[(int)enum_醫令資料.藥品名稱] = orderClasses[i].藥品名稱;
+                    value[(int)enum_醫令資料.病歷號] = orderClasses[i].病歷號;
+                    value[(int)enum_醫令資料.藥袋條碼] = orderClasses[i].藥袋條碼;
+                    value[(int)enum_醫令資料.病人姓名] = orderClasses[i].病人姓名;
+                    value[(int)enum_醫令資料.交易量] = orderClasses[i].交易量;
+                    value[(int)enum_醫令資料.開方日期] = orderClasses[i].開方日期;
+                    value[(int)enum_醫令資料.產出時間] = DateTime.Now.ToDateTimeString_6();
+                    value[(int)enum_醫令資料.過帳時間] = DateTime.MinValue.ToDateTimeString_6();
+                    value[(int)enum_醫令資料.狀態] = "未過帳";
                     list_value_Add.Add(value);
                 }
             }
@@ -139,30 +104,47 @@ namespace DB2VM
         }
 
 
-        [Route("full")]
+        [Route("full/{BarCode}")]
         [HttpGet]
         public string Getfull(string BarCode)
         {
-            if (BarCode.StringIsEmpty()) return $"BarCode value failed! {BarCode}";
-            if (BarCode.Length < 5) return $"BarCode value failed! {BarCode}";
+            returnData returnData = new returnData();
+            MyTimerBasic myTimerBasic = new MyTimerBasic(50000);
+            if (BarCode.StringIsEmpty())
+            {
+                returnData.Code = -200;
+                returnData.Result = $"BarCode value failed! {BarCode}";
+                return returnData.JsonSerializationt(true);
+            }
+            if (BarCode.Length < 5)
+            {
+                returnData.Code = -200;
+                returnData.Result = $"BarCode value failed! {BarCode}";
+                return returnData.JsonSerializationt(true);
+            }
             List<object[]> list_藥品資料 = this.sQLControl_UDSDBBCM.GetAllRows(null);
 
             string[] BarCode_Ary = BarCode.Split(';');
-
+            
             if (BarCode_Ary.Length < 5) return "BarCode error!";
             string 藥品碼 = BarCode_Ary[0].Substring(BarCode_Ary[0].Length - 5, 5);
+            string 床號 = BarCode_Ary[0].Substring(11, 5);
             string 日期_temp = BarCode_Ary[1];
             string 病人姓名 = BarCode_Ary[2];
             string 病歷號 = BarCode_Ary[3];
             string 交易量 = BarCode_Ary[4];
             if(交易量.StringIsInt32() == false)
             {
-                return $"交易量 error!{交易量}";
+                returnData.Code = -200;
+                returnData.Result = $"交易量 error!{交易量}";
+                return returnData.JsonSerializationt(true);
             }
             交易量 = (交易量.StringToInt32() * -1).ToString();
             if (日期_temp.Length != 7)
             {
-                return $"Date error!{日期_temp}";
+                returnData.Code = -200;
+                returnData.Result = $"Date error!{日期_temp}";
+                return returnData.JsonSerializationt(true);
             }
             else
             {
@@ -174,47 +156,51 @@ namespace DB2VM
             }
 
             list_藥品資料 = list_藥品資料.GetRows((int)enum_藥品資料_藥檔資料.藥品碼, 藥品碼);
-            if (list_藥品資料.Count == 0) return $"找不到此藥品 {藥品碼}";
+            if (list_藥品資料.Count == 0)
+            {
+                returnData.Code = -200;
+                returnData.Result = $"找不到此藥品 {藥品碼}";
+                return returnData.JsonSerializationt(true);
+            }
 
             List<object[]> list_value_Add = new List<object[]>();
             List<OrderClass> orderClasses = new List<OrderClass>();
             OrderClass orderClass = new OrderClass();
-            orderClass.PRI_KEY = Guid.NewGuid().ToString();
+            orderClass.PRI_KEY = $"{BarCode}";
             orderClass.藥局代碼 = "OPD";
-            orderClass.處方序號 = "";
             orderClass.藥袋條碼 = $"{BarCode}";
             orderClass.藥品碼 = 藥品碼;
             orderClass.藥品名稱 = list_藥品資料[0][(int)enum_藥品資料_藥檔資料.藥品名稱].ObjectToString();
             orderClass.病人姓名 = 病人姓名;
             orderClass.病歷號 = 病歷號;
-            orderClass.包裝單位 = list_藥品資料[0][(int)enum_藥品資料_藥檔資料.包裝單位].ObjectToString();
-            orderClass.劑量 = "";
+            orderClass.劑量單位 = list_藥品資料[0][(int)enum_藥品資料_藥檔資料.包裝單位].ObjectToString();
+            orderClass.床號 = 床號;
             orderClass.頻次 = "";
             orderClass.途徑 = "";
-            orderClass.天數 = "";
             orderClass.交易量 = 交易量;
-            orderClass.開方時間 = DateTime.Now.ToDateTimeString();
+            orderClass.開方日期 = DateTime.Now.ToDateTimeString();
             orderClasses.Add(orderClass);
 
             for (int i = 0; i < orderClasses.Count; i++)
             {
-                List<object[]> list_value = this.sQLControl_醫囑資料.GetRowsByDefult(null, enum_醫囑資料.PRI_KEY.GetEnumName(), orderClasses[i].PRI_KEY);
+                List<object[]> list_value = this.sQLControl_醫囑資料.GetRowsByDefult(null, enum_醫令資料.PRI_KEY.GetEnumName(), orderClasses[i].PRI_KEY);
                 if (list_value.Count == 0)
                 {
-                    object[] value = new object[new enum_醫囑資料().GetLength()];
-                    value[(int)enum_醫囑資料.GUID] = Guid.NewGuid().ToString();
-                    value[(int)enum_醫囑資料.PRI_KEY] = orderClasses[i].PRI_KEY;
-                    value[(int)enum_醫囑資料.藥局代碼] = orderClasses[i].藥局代碼;
-                    value[(int)enum_醫囑資料.藥品碼] = orderClasses[i].藥品碼;
-                    value[(int)enum_醫囑資料.藥品名稱] = orderClasses[i].藥品名稱;
-                    value[(int)enum_醫囑資料.病歷號] = orderClasses[i].病歷號;
-                    value[(int)enum_醫囑資料.藥袋條碼] = orderClasses[i].藥袋條碼;
-                    value[(int)enum_醫囑資料.病人姓名] = orderClasses[i].病人姓名;
-                    value[(int)enum_醫囑資料.交易量] = orderClasses[i].交易量;
-                    value[(int)enum_醫囑資料.開方日期] = orderClasses[i].開方時間;
-                    value[(int)enum_醫囑資料.產出時間] = DateTime.Now.ToDateTimeString_6();
-                    value[(int)enum_醫囑資料.過帳時間] = DateTime.MinValue.ToDateTimeString_6();
-                    value[(int)enum_醫囑資料.狀態] = "未過帳";
+                    object[] value = new object[new enum_醫令資料().GetLength()];
+                    value[(int)enum_醫令資料.GUID] = Guid.NewGuid().ToString();
+                    value[(int)enum_醫令資料.PRI_KEY] = orderClasses[i].PRI_KEY;
+                    value[(int)enum_醫令資料.藥局代碼] = orderClasses[i].藥局代碼;
+                    value[(int)enum_醫令資料.藥品碼] = orderClasses[i].藥品碼;
+                    value[(int)enum_醫令資料.藥品名稱] = orderClasses[i].藥品名稱;
+                    value[(int)enum_醫令資料.病歷號] = orderClasses[i].病歷號;
+                    value[(int)enum_醫令資料.藥袋條碼] = orderClasses[i].藥袋條碼;
+                    value[(int)enum_醫令資料.病人姓名] = orderClasses[i].病人姓名;
+                    value[(int)enum_醫令資料.交易量] = orderClasses[i].交易量;
+                    value[(int)enum_醫令資料.床號] = orderClasses[i].床號;
+                    value[(int)enum_醫令資料.開方日期] = orderClasses[i].開方日期;
+                    value[(int)enum_醫令資料.產出時間] = DateTime.Now.ToDateTimeString_6();
+                    value[(int)enum_醫令資料.過帳時間] = DateTime.MinValue.ToDateTimeString_6();
+                    value[(int)enum_醫令資料.狀態] = "未過帳";
                     list_value_Add.Add(value);
                 }
             }
@@ -223,7 +209,11 @@ namespace DB2VM
             {
                 this.sQLControl_醫囑資料.AddRows(null, list_value_Add);
             }
-            return orderClasses.JsonSerializationt();
+            returnData.Code = 200;
+            returnData.TimeTaken = myTimerBasic.ToString();
+            returnData.Result = $"取得醫令成功,共新增<{list_value_Add.Count}>筆資料";
+            returnData.Data = orderClasses;
+            return returnData.JsonSerializationt();
 
 
         }
